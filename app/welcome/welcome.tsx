996b0869c427812af9e4eb34da3741496ba66247
mi46460 <miqbal.mi1411@gmail.com>
2026-01-22 20:21:57 +0700

    feat: add many things

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -67,8 +67,8 @@ export function Welcome() {
     {
       category: "Fullstack",
       title: "Satukerja",
-      description:
-        "Satukerja is a comprehensive web and Android-based job portal platform designed to streamline the recruitment process and connect job seekers with career opportunities.",
+      description: `Satukerja is a comprehensive web and Android-based job portal platform designed to streamline 
+        the recruitment process and connect job seekers with career opportunities.`,
       technologies: [
         "Nextjs",
         "Material UI",
@@ -79,10 +79,16 @@ export function Welcome() {
         "Firebase",
         "Midtrans | Payment Gateway",
         "Docker",
+        "NGINX",
       ],
       details: {
-        overview:
-          "As a Fullstack Developer for Satukerja, I led the end-to-end development life cycle, from bridging client requirements with UI/UX design to executing full-scale technical implementation. I engineered a high-performance ecosystem using Next.js and Material UI for the frontend, coupled with a robust Go-based backend integrated with PostgreSQL, Redis caching, and Firebase authentication. My role also encompassed the integration of Midtrans for secure payments and managing scalable deployments across dev and production environments using Docker containerization.",
+        overview: `As a Fullstack Developer for Satukerja, I led the end-to-end development life cycle, 
+          from bridging client requirements with UI/UX design to executing full-scale technical implementation.
+           I engineered a high-performance ecosystem using Next.js and Material UI for the frontend, 
+           coupled with a robust Go-based backend integrated with PostgreSQL, Redis caching, and 
+           Firebase authentication. My role also encompassed the integration of Midtrans for secure payments 
+           and managing scalable deployments across dev and production environments using Docker c
+           ontainerization.`,
         responsibilities: [
           "Lead client coordination to define product requirements and roadmap, ensuring all developed features aligned with business objectives",
           "Acted as a technical liaison between the client and the UI/UX design team to ensure seamless design implementation across web and mobile platforms",
@@ -95,6 +101,40 @@ export function Welcome() {
           "The main challenge was creating a smooth, responsive chat interface that could handle real-time updates while maintaining good performance. Solved this by implementing efficient state management and lazy loading for chat history.",
       },
       link: "https://satukerja.id",
+    },
+    {
+      category: "Fullstack",
+      title: "Sipde",
+      description: `SIPDE is a web-based monitoring platform designed for the East Java Provincial Government 
+      to track the distribution and implementation of the People's Business Credit (KUR) fund. 
+      The system is integrated with two major regional banks in East Java responsible for fund disbursement.`,
+      technologies: [
+        "Symfony",
+        "Bootstrap",
+        "tailwind CSS",
+        "JQuery",
+        "Postgresql",
+        "Apache",
+      ],
+      details: {
+        overview: `As a Fullstack Developer for the SIPDE project, I played a pivotal role in developing a 
+        government-grade monitoring platform for the East Java Provincial Government to oversee the 
+        distribution of the People's Business Credit program. I was responsible for crafting a responsive 
+        and secure user interface using Tailwind CSS, while simultaneously engineering a sophisticated 
+        backend reporting engine. This system provides multi-dimensional data insights—ranging from 
+        institutional performance across bank branches to granular debtor-level tracking—enabling 
+        real-time monitoring of loan distributions, installment progress, and payment statuses through 
+        seamless integration with major regional banks.`,
+        responsibilities: [
+          "Developed the landing page and secure authentication modules using Tailwind CSS, ensuring a modern, responsive, and intuitive user interface.",
+          "Engineered a comprehensive reporting system by developing both the frontend components and robust backend logic to track credit distribution.",
+          "Developed data visualization and tracking metrics to monitor total loan amounts, outstanding balances, and real-time installment statuses.",
+          "Ensured data integrity and accuracy in displaying financial summaries, providing the government with clear insights into the credit program's performance.",
+        ],
+        challenges:
+          "The main challenge was creating a smooth, responsive chat interface that could handle real-time updates while maintaining good performance. Solved this by implementing efficient state management and lazy loading for chat history.",
+      },
+      link: "https://sipde.jatimprov.go.id/",
     },
   ];
 
@@ -109,7 +149,7 @@ export function Welcome() {
         <nav className="container mx-auto px-6 py-4">
           <div className="flex items-center justify-between">
             <h1 className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-              IQ
+              IQBAL
             </h1>
 
             {/* Desktop Menu */}
@@ -160,9 +200,9 @@ export function Welcome() {
         <div className="container mx-auto max-w-6xl">
           <div className="space-y-6">
             <div className="space-y-2">
-              <p className="text-blue-600 font-semibold text-lg">Hello, I'm</p>
+              <p className="text-blue-600 font-semibold text-2xl">Hello, I'm</p>
               <h1 className="text-5xl md:text-6xl font-bold text-gray-900">
-                Iqbal
+                Muhammad Iqbal
               </h1>
               <h2 className="text-3xl md:text-4xl font-semibold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
                 Full Stack Developer
@@ -184,7 +224,7 @@ export function Welcome() {
                 View Projects
               </a>
               <a
-                href="./cv_muhammad_iqbal.pdf"
+                href="/cv_muhammad_iqbal.pdf"
                 className="px-6 py-3 border-2 border-blue-600 text-blue-600 rounded-lg font-semibold hover:bg-blue-50 transition-all flex items-center content-between cursor-pointer"
                 download={true}
               >
@@ -293,8 +333,8 @@ export function Welcome() {
                     Implement authentication using Firebase, JWT
                   </li>
                   <li className="text-gray-700 leading-relaxed text-lg">
-                    Integrated RESTful APIs and ensured smooth client–server
-                    data flow
+                    Integrate RESTful APIs and ensured smooth client–server data
+                    flow
                   </li>
                   <li className="text-gray-700 leading-relaxed text-lg">
                     Implement deployment process across dev and prod
@@ -328,15 +368,16 @@ export function Welcome() {
                 </p>
                 <ul className="space-y-2 list-disc pl-5 mt-2">
                   <li className="text-gray-700 leading-relaxed text-lg">
-                    Collaborate closely with UI/UX designers to translate design
-                    mockups into pixel-perfect and responsive mobile interfaces.
+                    Collaborated closely with UI/UX designers to translate
+                    design mockups into pixel-perfect and responsive mobile
+                    interfaces.
                   </li>
                   <li className="text-gray-700 leading-relaxed text-lg">
-                    Develop comprehensive unit tests for every new feature to
+                    Developed comprehensive unit tests for every new feature to
                     ensure code reliability and maintain high software quality.
                   </li>
                   <li className="text-gray-700 leading-relaxed text-lg">
-                    Integrate RESTful APIs/GraphQL with backend services to
+                    Integrated RESTful APIs/GraphQL with backend services to
                     ensure seamless data flow and application functionality.
                   </li>
                 </ul>
@@ -543,45 +584,3 @@ export function Welcome() {
     </div>
   );
 }
-
-const resources = [
-  {
-    href: "https://reactrouter.com/docs",
-    text: "React Router Docs",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 24 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M15.0686 1.25995L14.5477 1.17423L14.2913 1.63578C14.1754 1.84439 14.0545 2.08275 13.9422 2.31963C12.6461 2.16488 11.3406 2.16505 10.0445 2.32014C9.92822 2.08178 9.80478 1.84975 9.67412 1.62413L9.41449 1.17584L8.90333 1.25995C7.33547 1.51794 5.80717 1.99419 4.37748 2.66939L4.19 2.75793L4.07461 2.93019C1.23864 7.16437 0.46302 11.3053 0.838165 15.3924L0.868838 15.7266L1.13844 15.9264C2.81818 17.1714 4.68053 18.1233 6.68582 18.719L7.18892 18.8684L7.50166 18.4469C7.96179 17.8268 8.36504 17.1824 8.709 16.4944L8.71099 16.4904C10.8645 17.0471 13.128 17.0485 15.2821 16.4947C15.6261 17.1826 16.0293 17.8269 16.4892 18.4469L16.805 18.8725L17.3116 18.717C19.3056 18.105 21.1876 17.1751 22.8559 15.9238L23.1224 15.724L23.1528 15.3923C23.5873 10.6524 22.3579 6.53306 19.8947 2.90714L19.7759 2.73227L19.5833 2.64518C18.1437 1.99439 16.6386 1.51826 15.0686 1.25995ZM16.6074 10.7755L16.6074 10.7756C16.5934 11.6409 16.0212 12.1444 15.4783 12.1444C14.9297 12.1444 14.3493 11.6173 14.3493 10.7877C14.3493 9.94885 14.9378 9.41192 15.4783 9.41192C16.0471 9.41192 16.6209 9.93851 16.6074 10.7755ZM8.49373 12.1444C7.94513 12.1444 7.36471 11.6173 7.36471 10.7877C7.36471 9.94885 7.95323 9.41192 8.49373 9.41192C9.06038 9.41192 9.63892 9.93712 9.6417 10.7815C9.62517 11.6239 9.05462 12.1444 8.49373 12.1444Z"
-          strokeWidth="1.5"
-        />
-      </svg>
-    ),
-  },
-];
