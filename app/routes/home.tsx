02aaa0ecabc0421f00cca866e88e7685937d48a6
mi46460 <miqbal.mi1411@gmail.com>
2026-01-24 20:04:59 +0700

    feat: change web title

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Muhammad Iqbal" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
